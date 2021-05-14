b9a519ee4aaa47ed9fdf8789c356fc4c47817646
Martin Rudoff <martin.rudoff@tomra.com>
2021-05-14 12:40:52 +0200

    tidy: cleanup code

--- src/gpugraph/RenderTarget.cpp
+++ src/gpugraph/RenderTarget.cpp
@@ -90,7 +90,7 @@ namespace gpugraph
     RenderTarget::~RenderTarget()
     {
         clear();
-        glDeleteBuffers(1, &_vbo);
+        glDeleteBuffers(1, &_vertex_buffer);
     }
 
     std::size_t RenderTarget::to_index(std::size_t x, std::size_t y)
@@ -115,17 +115,17 @@ namespace gpugraph
         _tile_count_y = (height + tw - 1) / tw;
 
         auto tile_count = static_cast<std::size_t>(_tile_count_x * _tile_count_y);
-        _fbos.resize(tile_count);
-        _textures.resize(tile_count);
-        _depth.resize(tile_count);
+        _framebuffer_objects.resize(tile_count);
+        _texture_attachments.resize(tile_count);
+        _depth_attachments.resize(tile_count);
 
-        glGenFramebuffers(static_cast<GLsizei>(tile_count), _fbos.data());
-        glGenTextures(static_cast<GLsizei>(tile_count), _textures.data());
-        glGenRenderbuffers(static_cast<GLsizei>(tile_count), _depth.data());
-        for (std::size_t i = 0; i < _fbos.size(); ++i)
+        glGenFramebuffers(static_cast<GLsizei>(tile_count), _framebuffer_objects.data());
+        glGenTextures(static_cast<GLsizei>(tile_count), _texture_attachments.data());
+        glGenRenderbuffers(static_cast<GLsizei>(tile_count), _depth_attachments.data());
+        for (std::size_t i = 0; i < _framebuffer_objects.size(); ++i)
         {
-            auto texture_id = _textures.at(i);
-            auto fbo_id = _fbos.at(i);
+            auto texture_id = _texture_attachments.at(i);
+            auto fbo_id = _framebuffer_objects.at(i);
 
             glBindFramebuffer(GL_FRAMEBUFFER, fbo_id);
             glBindTexture(GL_TEXTURE_2D, texture_id);
@@ -140,11 +140,11 @@ namespace gpugraph
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
             glFramebufferTexture2D(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, texture_id, 0);
 
-            glBindRenderbuffer(GL_RENDERBUFFER, _depth.at(i));
+            glBindRenderbuffer(GL_RENDERBUFFER, _depth_attachments.at(i));
             glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH_COMPONENT24,
                 static_cast<GLsizei>(_tile_width),
                 static_cast<GLsizei>(_tile_width));
-            glFramebufferRenderbuffer(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _depth.at(i));
+            glFramebufferRenderbuffer(GL_DRAW_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_RENDERBUFFER, _depth_attachments.at(i));
             if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
             {
                 throw std::runtime_error("failed to create render target, framebuffer incomplete");
@@ -152,7 +152,7 @@ namespace gpugraph
         }
 
         //        glBindFramebuffer(GL_FRAMEBUFFER, 0);
-        _tiles.resize(_fbos.size());
+        _tiles.resize(_framebuffer_objects.size());
         std::vector<GLfloat> vertices;
         std::vector<GLushort> indices;
         // 1 quad for each tile..
@@ -186,8 +186,8 @@ namespace gpugraph
                     rect(x1, y1, x2, y2));
             }
         }
-        glGenBuffers(1, &_vbo);
-        glBindBuffer(GL_ARRAY_BUFFER, _vbo);
+        glGenBuffers(1, &_vertex_buffer);
+        glBindBuffer(GL_ARRAY_BUFFER, _vertex_buffer);
         if (vertices.size() > 0)
             glBufferData(GL_ARRAY_BUFFER, vertices.size() * sizeof(float), vertices.data(), GL_STATIC_DRAW);
         glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -206,13 +206,6 @@ namespace gpugraph
             glVertex3f(r.x2, r.y1, 0.0f);
             glEnd();
         }
-        glColor4f(1.f, 0.f, 0.f, 1.f);
-        glBegin(GL_LINE_LOOP);
-        glVertex3f(2, 2, 0.0f);
-        glVertex3f(2, 102, 0.0f);
-        glVertex3f(102, 102, 0.0f);
-        glVertex3f(102, 2, 0.0f);
-        glEnd();
     }
 
     void RenderTarget::blit(glm::mat4 const& mat)
@@ -223,12 +216,13 @@ namespace gpugraph
 
     void RenderTarget::clear()
     {
-        glDeleteFramebuffers(static_cast<GLsizei>(_fbos.size()), _fbos.data());
-        glDeleteTextures(static_cast<GLsizei>(_textures.size()), _textures.data());
-        glDeleteRenderbuffers(static_cast<GLsizei>(_depth.size()), _depth.data());
+        glDeleteFramebuffers(static_cast<GLsizei>(_framebuffer_objects.size()), _framebuffer_objects.data());
+        glDeleteTextures(static_cast<GLsizei>(_texture_attachments.size()), _texture_attachments.data());
+        glDeleteRenderbuffers(static_cast<GLsizei>(_depth_attachments.size()), _depth_attachments.data());
+
         _tiles.clear();
-        _fbos.clear();
-        _textures.clear();
+        _framebuffer_objects.clear();
+        _texture_attachments.clear();
         _tile_count_x = _tile_count_y = 0;
     }
 
@@ -256,7 +250,7 @@ namespace gpugraph
     {
         glDisable(GL_TEXTURE_2D);
         glDisable(GL_DEPTH_TEST);
-        auto fbo_id = _render_target->_fbos.at(_base_index);
+        auto fbo_id = _render_target->_framebuffer_objects.at(_base_index);
         glBindFramebuffer(GL_FRAMEBUFFER, fbo_id);
         glViewport(0, 0,
             static_cast<GLsizei>(_rectangle.width()),
@@ -274,13 +268,13 @@ namespace gpugraph
 
         program.transform = transform;
         program.texture = 0;
-        glBindBuffer(GL_ARRAY_BUFFER, _render_target->_vbo);
+        glBindBuffer(GL_ARRAY_BUFFER, _render_target->_vertex_buffer);
         program.position.set(2, GL_FLOAT, GL_FALSE, sizeof(float) * 4, 0);
         program.texture_coord.set(2, GL_FLOAT, GL_FALSE, sizeof(float) * 4, sizeof(float) * 2);
         
         glActiveTexture(GL_TEXTURE0);
         glEnable(GL_TEXTURE_2D);
-        auto texture_id = _render_target->_textures.at(_base_index);
+        auto texture_id = _render_target->_texture_attachments.at(_base_index);
         glBindTexture(GL_TEXTURE_2D, texture_id);
 
         glDrawArrays(GL_TRIANGLE_STRIP, static_cast<GLint>(_base_index) * 4, 4);
--- src/gpugraph/RenderTarget.h
+++ src/gpugraph/RenderTarget.h
@@ -17,13 +17,16 @@ namespace gpugraph
         struct Tile
         {
         public:
+            /// :-)
             Tile(RenderTarget *, std::size_t base_index, rect x);
+
+            /// tile vertices
             rect const& rectangle() const;
             
-            /// render to tile. binds the fbo and sets
-            /// the viewport to rect.width(), rect.height()
-            /// before the call to render_func
+            /// render to tile (gl)
             void render(std::function<void()> render_func);
+
+            /// draw tile with some transformation applied
             void blit(glm::mat4 const&);
 
         private:
@@ -32,24 +35,35 @@ namespace gpugraph
             rect _rectangle;
         };
 
+        /// :-)
         RenderTarget(
             std::size_t width=0, 
             std::size_t height=0,
             std::size_t tile_width=256,
             std::size_t overlap=1);
-
+        
+        /// :-)
         ~RenderTarget();
 
+        /// resize the target. will only happen if width or height was changed
         void set_size(std::size_t width, std::size_t height);
+       
+        /// :-)
         std::array<std::size_t, 2> size() const;
 
-        Tile& tile(std::size_t x, std::size_t y);
+        /// get tile by tile position
+        Tile& tile(std::size_t index_x, std::size_t index_y);
 
-        std::size_t tile_count() const;
+        /// :-)
         std::vector<std::unique_ptr<Tile>> const& tiles() const;
 
+        /// :-)
+        std::size_t tile_count() const;
+
+        /// outputs tile vertices as GL_LINE_LOOP to gl
         void _debug_draw();
 
+        /// draw the whole tiling, which is actually a plane, with some transformation applied
         void blit(glm::mat4 const&);
 
     private:
@@ -60,12 +74,16 @@ namespace gpugraph
         std::size_t _height = 0;
         std::size_t _tile_width = 0;
         std::size_t _overlap = 0;
+
         std::size_t _tile_count_x = 0;
         std::size_t _tile_count_y = 0;
-        std::vector<GLuint> _fbos;
-        std::vector<GLuint> _depth;
-        std::vector<GLuint> _textures;
-        GLuint _vbo;
+        
+        std::vector<GLuint> _framebuffer_objects;
+        std::vector<GLuint> _depth_attachments;
+        std::vector<GLuint> _texture_attachments;
+
+        GLuint _vertex_buffer;
+
         std::vector<std::unique_ptr<Tile>> _tiles;
     };
 
