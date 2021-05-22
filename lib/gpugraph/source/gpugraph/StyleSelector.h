d3158843c58f1cb0121816d7798d448e2209e039
Martin Rudoff <martin.rudoff@tomra.com>
2021-05-22 11:54:53 +0200

    tidy: cleanup doc

--- lib/gpugraph/source/gpugraph/StyleSelector.h
+++ lib/gpugraph/source/gpugraph/StyleSelector.h
@@ -14,7 +14,6 @@ namespace gpugraph
     class StyleSelector
     {
     public:
-
         struct PathItem 
         {
             using PseudoSelector = std::function<bool(Node const&)>;
@@ -22,24 +21,19 @@ namespace gpugraph
             StyleHash style_hash;
             std::vector<PseudoSelector> pseudo_selectors;
         };
-
         using Path = std::vector <PathItem>;
 
         StyleSelector(css::selector const&);
-
-        std::uint64_t specificity() const;
-
-        Path const& path() const;
-
-        bool is_selecting(Node const&) const;
-
         css::selector const& definition() const;
+
+        // https://www.w3.org/TR/2011/REC-CSS2-20110607/cascade.html#specificity
+        std::uint64_t specificity() const;
+        Path const& path() const;
+        bool is_selecting(Node const&) const;
 
     private:
         css::selector _definition;
-        // this is the compiled resp. reversed path of the definition
         Path _path;
-        // https://www.w3.org/TR/2011/REC-CSS2-20110607/cascade.html#specificity (we ignore spec. "a")
         std::uint64_t _specificity = 0;
     };
 
